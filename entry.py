270d2815fa4bd5e539cd34dde9579646f2ed7084
P4SSER8Y <P4SSER8Y@outlook.com>
2022-10-24 21:47:14 +0800

    🎨 use launch.yml to configure initialization

--- entry.py
+++ entry.py
@@ -3,25 +3,47 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s][%(levelname)s][%(threadName)s] %(message)s")
 # autopep8: off
 
-from sorting_agent import SortingAgent
 import click
+from sorting_agent import SortingAgent
 import signal
+import yaml
 
-sorting_agent = SortingAgent()
-
+threads = []
 
 def handler_SIGINT(signum, frame):
     logging.warning("SIGINT raised")
-    sorting_agent.require_quit()
+    for item in threads:
+        if hasattr(item, "require_quit"):
+            item.require_quit()
+
+
+def factory(object: dict):
+    _MAP_ = {
+        "sorting_agent": SortingAgent,
+    }
+    logging.debug(object)
+
+    ret = None
+    if not object["type"].lower() in _MAP_.keys():
+        return None
+    ret = _MAP_[object["type"]](name=object["name"])
+    if hasattr(ret, "load_config") and "config" in object.keys():
+        ret.load_config(object["config"])
+    return ret
 
 
 @click.command()
-@click.argument("sorting_config", default="./sorting_config.yml", type=click.Path(exists=True))
-def main(sorting_config):
+@click.argument("config", default="./launch.yml", type=click.Path(exists=True))
+def main(config):
     signal.signal(signal.SIGINT, handler_SIGINT)
-    sorting_agent.load_config(sorting_config)
-    sorting_agent.start()
-    sorting_agent.join()
+    with open(config, "r") as f:
+        items = yaml.load(f, yaml.FullLoader)
+    for item in items:
+        object = factory(item)
+        if object:
+            threads.append(object)
+    [x.start() for x in threads]
+    [x.join() for x in threads]
 
 
 if __name__ == "__main__":
