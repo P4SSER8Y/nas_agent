6624e9ee8ad6d85194bc102a940cc5f50f056854
P4SSER8Y <P4SSER8Y@outlook.com>
2022-10-27 00:06:23 +0800

    📖 add document printing

--- entry.py
+++ entry.py
@@ -3,11 +3,16 @@ from rich.logging import RichHandler
 import click
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command(help="takeoff and start")
 @click.argument("config", default="./launch.yml", type=click.Path(exists=True))
 @click.option("-l", "--log-level", default="info",
               type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR"], case_sensitive=False))
-def main(config, log_level):
+def takeoff(config, log_level):
     MAP_LOG_LEVEL = {
         "DEBUG": logging.DEBUG,
         "INFO": logging.INFO,
@@ -22,6 +27,21 @@ def main(config, log_level):
 
     from headquarter import entry
     entry(config)
+
+
+@main.command(help="list all available processors of sorting agent")
+def list_processors():
+    from sorting_agent import processes
+    from rich.console import Console
+    from rich.markdown import Markdown
+    m = processes.ProcessMap
+    md = []
+    for item in m.keys():
+        md.append(f"# {item}")
+        if m[item].__doc__:
+            md.append(m[item].__doc__)
+        md.append('')
+    Console().print(Markdown("\n".join(md)))
 
 
 if __name__ == "__main__":
--- sorting_agent/processes.py
+++ sorting_agent/processes.py
@@ -23,6 +23,12 @@ def wrapper(func):
 
 @wrapper
 async def delay(context, arg):
+    """delay some time
+
+    input: None
+    arg: time to be delayed, in second
+    output: None
+    """
     ts = float(arg)
     logging.debug(f"delay {ts}")
     await asyncio.sleep(ts)
@@ -30,7 +36,13 @@ async def delay(context, arg):
 
 
 @wrapper
-def chown_to_parent(context, arg: str):
+def chown_to_parent(context, arg):
+    """chown to the uid/pid of parent
+
+    input: source
+    arg: None
+    output: None
+    """
     path = context["source"]
     os.chown(path, path.parent.stat().st_uid, path.parent.stat().st_gid)
     return context
@@ -38,6 +50,14 @@ def chown_to_parent(context, arg: str):
 
 @wrapper
 def mkpath(context, arg: str):
+    """make path
+
+    the owners of all directories are set to the uid/pid of parent
+    the permission of all directories are set to 0o774
+    input: destination
+    arg: None
+    output: None
+    """
     def iter(path: pathlib.Path):
         if path.exists():
             return True
@@ -59,6 +79,12 @@ def mkpath(context, arg: str):
 
 @wrapper
 def move(context, arg: str):
+    """move file to destination
+
+    input: source, and fields in format argument
+    arg: string, with format of destination
+    output: source, destination
+    """
     context["destination"] = pathlib.Path(arg.format(**context)).absolute().resolve()
     context = mkpath(context, arg)
     if context:
@@ -71,17 +97,35 @@ def move(context, arg: str):
 
 @wrapper
 def debug_info(context, arg):
+    """print debug infomation
+
+    input: None
+    arg: None
+    output: None
+    """
     logging.info(f"{context}")
     return context
 
 
 @wrapper
 def failure(context, arg):
+    """trigger failure and drop everything
+
+    input: None
+    arg: None
+    output: None
+    """
     return None
 
 
 @wrapper
 def skip_directory(context, arg):
+    """trigger failure if is directory
+
+    input: is_dir
+    arg: None
+    output: None
+    """
     if context["is_dir"]:
         return None
     else:
@@ -90,6 +134,12 @@ def skip_directory(context, arg):
 
 @wrapper
 def parse_filename(context, arg):
+    """parse file name and get relative information
+
+    input: source
+    arg: None
+    output: parent, relative_parent, suffix, stem
+    """
     context["parent"] = context["source"].parent
     context["relative_parent"] = context["relative_path"].parent
     context["suffix"] = ''.join(context["source"].suffixes)
@@ -99,4 +149,10 @@ def parse_filename(context, arg):
 
 @wrapper
 async def get_md5(context, arg):
+    """calculate MD5 hash value with file's content
+
+    input: source
+    arg: None
+    output: md5
+    """
     pass
